da60fa6cd1ad49649a2ebf106fa4b592b0d09367
iali22 <ali_mahde22@yahoo.com>
2021-06-02 16:58:32 +0200

    add icons to sidebar

--- frontend/src/container/Layout/Layout.jsx
+++ frontend/src/container/Layout/Layout.jsx
@@ -5,8 +5,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Divider from "@material-ui/core/Divider";
 import Drawer from "@material-ui/core/Drawer";
 import Hidden from "@material-ui/core/Hidden";
-import IconButton from "@material-ui/core/IconButton";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
+
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
@@ -20,6 +19,12 @@ import { connect } from "react-redux";
 import { LogoutMenu } from "../../components";
 import { NavLink } from "react-router-dom";
 import style from "./Layout.module.css";
+
+// Icons
+import IconButton from "@material-ui/core/IconButton";
+import HomeIcon from "@material-ui/icons/Home";
+import PeopleIcon from "@material-ui/icons/People";
+
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
@@ -76,13 +81,13 @@ function Layout({ window, children, user }) {
       text: "Home",
       role: "admin",
       route: "/",
-      icon: <InboxIcon />,
+      icon: <HomeIcon />,
     },
     {
       text: "Students",
       role: "admin",
       route: "/students",
-      icon: <InboxIcon />,
+      icon: <PeopleIcon />,
     },
   ];
 
