7284384eabdb19e5581be9701196fbe41698ce3d
iali22 <ali_mahde22@yahoo.com>
2021-06-07 00:05:25 +0200

    add charts to home page

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,3 +1,4 @@
+import { Grid, Typography } from "@material-ui/core";
 import React from "react";
 import { StudentsChart } from "../components";
 import { Layout } from "../container";
@@ -5,11 +6,40 @@ import { Layout } from "../container";
 function Home() {
   return (
     <Layout>
-      <h1>Welcome in FSSR Enrollment System</h1>
+      <Typography variant="h2" component="h2" color="textPrimary">
+        Welcome in FSSR Enrollment System{" "}
+      </Typography>
+      <Typography
+        variant="h5"
+        component="h5"
+        color="textSecondary"
+        style={{
+          marginBottom: "1rem",
+        }}
+      >
+        Some Facts about our college
+      </Typography>
 
-      <div>
-        <StudentsChart />
-      </div>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Typography variant="h6" component="h5" color="textSecondary">
+            Number Of Students
+          </Typography>
+          <StudentsChart />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h6" component="h5" color="textSecondary">
+            Number Of Departments
+          </Typography>
+          <StudentsChart />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h6" component="h5" color="textSecondary">
+            Number Of Subjects
+          </Typography>
+          <StudentsChart />
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
