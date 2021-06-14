fc43c6533277e927d632878b42b5eaee3515fbb3
Fady Beshay (TAC-EGY) <f84093296@china.huawei.com>
2021-06-14 22:42:23 +0200

    Update student enrol object

--- frontend/src/components/Enrollments/StudentEnrollments.jsx
+++ frontend/src/components/Enrollments/StudentEnrollments.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import {
-  LoadStudentEnrollments,
-  SelectedCourse,
+	LoadStudentEnrollments,
+	SelectedCourse
 } from "../../redux/actions/enrollmentsActions";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -22,124 +22,124 @@ import { Chip, Typography } from "@material-ui/core";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const useStyles = makeStyles((theme) => ({
-  table: {
-    minWidth: 650,
-  },
-  iconButton: {
-    padding: 10,
-  },
-  divider: {
-    height: 28,
-    margin: 4,
-  },
-  studentContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingTop: "10px",
-  },
-  spaceMargin: {
-    marginBottom: "10px",
-  },
+	table: {
+		minWidth: 650
+	},
+	iconButton: {
+		padding: 10
+	},
+	divider: {
+		height: 28,
+		margin: 4
+	},
+	studentContainer: {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		paddingTop: "10px"
+	},
+	spaceMargin: {
+		marginBottom: "10px"
+	}
 }));
 
 function StudentEnrollments() {
-  const classes = useStyles();
-  const [selectedCourses, setSelectedCourses] = useState([]);
-  const isLoading = useSelector((state) => state.enrollment.isLoading);
-  const dispatch = useDispatch();
-  const enrollment = useSelector((state) => state.enrollment.enrollment);
+	const classes = useStyles();
+	const [selectedCourses, setSelectedCourses] = useState([]);
+	const isLoading = useSelector((state) => state.enrollment.isLoading);
+	const dispatch = useDispatch();
+	const enrollment = useSelector((state) => state.enrollment.enrollment);
 
-  useEffect(() => {
-    dispatch(LoadStudentEnrollments());
-  }, []);
+	useEffect(() => {
+		dispatch(LoadStudentEnrollments());
+	}, []);
 
-  const selectCourse = (id) => {
-    setSelectedCourses((prev) => [...prev, id]);
-  };
+	const selectCourse = (id) => {
+		setSelectedCourses((prev) => [...prev, id]);
+	};
 
-  const sendIds = (ids) => {
-    dispatch(SelectedCourse({ courses: ids }));
-    setSelectedCourses([]);
-  };
+	const sendIds = (ids) => {
+		dispatch(SelectedCourse({ courses: ids }));
+		setSelectedCourses([]);
+	};
 
-  return (
-    <div>
-      <Typography
-        variant="h6"
-        component="p"
-        color="textSecondary"
-        className={classes.spaceMargin}
-      >
-        Select Your Courses
-      </Typography>
+	return (
+		<div>
+			<Typography
+				variant="h6"
+				component="p"
+				color="textSecondary"
+				className={classes.spaceMargin}
+			>
+				Select Your Courses
+			</Typography>
 
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="left">Code</TableCell>
-              <TableCell align="left">title</TableCell>
-              <TableCell align="left">credit</TableCell>
-              <TableCell align="left">instructor</TableCell>
-              <TableCell align="left">status</TableCell>
-              <TableCell align="center" colSpan={2}>
-                Actions
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {!isLoading &&
-              enrollment &&
-              enrollment.map((enroll) => (
-                <TableRow key={enroll._id}>
-                  <TableCell align="left">{enroll.code}</TableCell>
-                  <TableCell align="left">{enroll.title}</TableCell>
-                  <TableCell align="left">{enroll.credit}</TableCell>
-                  <TableCell align="left">{enroll.instructor}</TableCell>
-                  <TableCell align="left">
-                    {enroll.selected ? (
-                      <Chip label="Selected" color="primary" />
-                    ) : (
-                      <Chip label="Not Selected" color="secondary" />
-                    )}
-                  </TableCell>
+			<TableContainer component={Paper}>
+				<Table className={classes.table} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell align="left">Code</TableCell>
+							<TableCell align="left">title</TableCell>
+							<TableCell align="left">credit</TableCell>
+							<TableCell align="left">instructor</TableCell>
+							<TableCell align="left">status</TableCell>
+							<TableCell align="center" colSpan={2}>
+								Actions
+							</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{!isLoading &&
+							enrollment.courses &&
+							enrollment.courses.map((enroll) => (
+								<TableRow key={enroll._id}>
+									<TableCell align="left">{enroll.code}</TableCell>
+									<TableCell align="left">{enroll.title}</TableCell>
+									<TableCell align="left">{enroll.credit}</TableCell>
+									<TableCell align="left">{enroll.instructor}</TableCell>
+									<TableCell align="left">
+										{enroll.selected ? (
+											<Chip label="Selected" color="primary" />
+										) : (
+											<Chip label="Not Selected" color="secondary" />
+										)}
+									</TableCell>
 
-                  <TableCell align="left">
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          onClick={() => selectCourse(enroll._id)}
-                          color="primary"
-                        />
-                      }
-                      label="Selected"
-                    />
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      {selectedCourses.length > 0 && (
-        <div className={classes.studentContainer}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => sendIds(selectedCourses)}
-          >
-            Send
-          </Button>
-        </div>
-      )}
+									<TableCell align="left">
+										<FormControlLabel
+											control={
+												<Checkbox
+													onClick={() => selectCourse(enroll._id)}
+													color="primary"
+												/>
+											}
+											label="Selected"
+										/>
+									</TableCell>
+								</TableRow>
+							))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+			{selectedCourses.length > 0 && (
+				<div className={classes.studentContainer}>
+					<Button
+						variant="contained"
+						color="primary"
+						onClick={() => sendIds(selectedCourses)}
+					>
+						Send
+					</Button>
+				</div>
+			)}
 
-      {isLoading && (
-        <div style={{ textAlign: "center" }}>
-          <CircularProgress disableShrink />
-        </div>
-      )}
-    </div>
-  );
+			{isLoading && (
+				<div style={{ textAlign: "center" }}>
+					<CircularProgress disableShrink />
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default StudentEnrollments;
